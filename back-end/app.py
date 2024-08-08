6101fe1d5367d4265d76f7bb471ef8ffdec377e4
Brad Steinberg <steinbergbrad@gmail.com>
2024-08-07 21:03:12 -0400

    introduced code that I want sonarcloud to catch as unused

--- back-end/app.py
+++ back-end/app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/cards', methods=['GET'])
+def cards():
+    cards = [ 'A', '1', '2' ]
+    return 'HAACHOO'
+
 if __name__ == '__main__':
     app.run(debug=True)
 
